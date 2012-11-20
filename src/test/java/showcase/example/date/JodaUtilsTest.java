579f07163ffb29f9b6677ecf475839929a6d4f47
zfc827 <46919969>
2012-11-20 21:34:58 +0800

    添加了 joda-time 库的使用演示。

--- src/test/java/showcase/example/date/JodaUtilsTest.java
+++ src/test/java/showcase/example/date/JodaUtilsTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.text.SimpleDateFormat;
 import java.util.Locale;
 
 /**
@@ -19,10 +18,10 @@ public class JodaUtilsTest {
     private JodaUtils jodaUtils = new JodaUtils();
 
     /**
-     *  演示 DateTime 对象基本属性
+     * 演示 DateTime 对象基本属性
      */
     @Test
-    public void testDateTimeProperty () {
+    public void testDateTimeProperty() {
         DateTime now = DateTime.now();
         logger.debug("世纪：{}", String.valueOf(now.getCenturyOfEra()));
         logger.debug("年代：{}", String.valueOf(now.getYearOfEra()));
@@ -48,11 +47,11 @@ public class JodaUtilsTest {
     public void testCalculate() {
         // 计算2012年每个月的第一个周六的日期
         jodaUtils = new JodaUtils(2012, 1, 1, 0, 0, 0);
-        for(int i = 1; i <= 12; i++){
+        for (int i = 1; i <= 12; i++) {
             // 获取每个月第一个星期的周六
             DateTime dateTime = jodaUtils.setMonth(i).dayOfMonth().withMinimumValue().dayOfWeek().setCopy(6);
             // 判断一个星期是否跨月
-            if(dateTime.getMonthOfYear() != i) {
+            if (dateTime.getMonthOfYear() != i) {
                 // 如果跨月增加一个星期
                 dateTime = dateTime.plusWeeks(1);
             }
