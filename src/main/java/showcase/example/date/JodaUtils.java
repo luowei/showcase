a57d634fe7d37ea77cc232cf358eb804da5934f3
zfc827 <46919969>
2012-11-20 21:29:32 +0800

    添加了 joda-time 库的使用演示。

---
+++ src/main/java/showcase/example/date/JodaUtils.java
@@ -0,0 +1,137 @@
+package showcase.example.date;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * 演示 Joda
+ * @author zfc827@gmail.com
+ */
+public class JodaUtils {
+
+    private final Logger logger = LoggerFactory.getLogger(JodaUtils.class);
+
+    /**
+     * joda 封装的日期对象，类似 java.util.Date
+     */
+    private DateTime dateTime;
+
+    public JodaUtils() {
+        this.dateTime = DateTime.now();
+    }
+
+    /**
+     * 创建一个指定 年月日/时分秒 DateTime 对象。
+     * @param year
+     * @param month
+     * @param day
+     * @param hours
+     * @param minute
+     * @param second
+     */
+    public JodaUtils(int year, int month, int day, int hours, int minute, int second) {
+        this.dateTime = new DateTime(year, month, day, hours, minute, second);
+    }
+
+    public DateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(DateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    /**
+     * 返回以当前 DateTime 对象表示的时间，减去指定的天份数后的 DateTime 对象
+     * @param day
+     * @return
+     */
+    public DateTime minusDays(int day) {
+        return dateTime.minusDays(day);
+    }
+
+    /**
+     * * 返回以当前 DateTime 对象表示的时间，减去指定的月份数后的 DateTime 对象
+     * @param month
+     * @return
+     */
+    public DateTime minusMonth(int month) {
+        return dateTime.minusMonths(month);
+    }
+
+    /**
+     * * 返回以当前 DateTime 对象表示的时间，减去指定的年份数后的 DateTime 对象
+     * @param year
+     * @return
+     */
+    public DateTime minusYear(int year) {
+        return dateTime.minusMonths(year);
+    }
+
+    /**
+     * 返回当月最大的一天的 DateTime 对象
+     * @return
+     */
+    public DateTime maxDayOfMonth() {
+        return dateTime.dayOfMonth().withMaximumValue();
+    }
+
+    /**
+     * 返回当月最小的一天的 DateTime 对象
+     * @return
+     */
+    public DateTime minDayOfMonth() {
+        return dateTime.dayOfMonth().withMinimumValue();
+    }
+
+    /**
+     * 返回指定年份的 DateTime 对象
+     * @param year
+     * @return
+     */
+    public DateTime setYear(int year) {
+        return dateTime.year().setCopy(year);
+    }
+
+    /**
+     * 返回指定月份的 DateTime 对象
+     * @param month
+     * @return
+     */
+    public DateTime setMonth(int month) {
+        return dateTime.monthOfYear().setCopy(month);
+    }
+
+    /**
+     * 返回指定星期几的 DateTime 对象
+     * @param dayOfWeek
+     * @return
+     */
+    public DateTime setDayOfWeek(int dayOfWeek) {
+        return dateTime.dayOfWeek().setCopy(dayOfWeek);
+    }
+
+    /**
+     * 返回指定当月第几天的 DateTime 对象
+     * @param dayOfMonth
+     * @return
+     */
+    public DateTime setDayOfMonth(int dayOfMonth) {
+        return dateTime.dayOfMonth().setCopy(dayOfMonth);
+    }
+
+    /**
+     * 返回指定当年第几天的 DateTime 对象
+     * @param dayOfYear
+     * @return
+     */
+    public DateTime setDayOfYear(int dayOfYear) {
+        return dateTime.dayOfYear().setCopy(dayOfYear);
+    }
+
+
+}
---
+++ src/test/java/showcase/example/date/JodaUtilsTest.java
@@ -0,0 +1,74 @@
+package showcase.example.date;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * @author zfc827@gmail.com
+ */
+public class JodaUtilsTest {
+
+    private final Logger logger = LoggerFactory.getLogger(JodaUtilsTest.class);
+
+    private JodaUtils jodaUtils = new JodaUtils();
+
+    /**
+     *  演示 DateTime 对象基本属性
+     */
+    @Test
+    public void testDateTimeProperty () {
+        DateTime now = DateTime.now();
+        logger.debug("世纪：{}", String.valueOf(now.getCenturyOfEra()));
+        logger.debug("年代：{}", String.valueOf(now.getYearOfEra()));
+        logger.debug("月：{}", String.valueOf(now.getMonthOfYear()));
+        logger.debug("当年第{}周", String.valueOf(now.getWeekOfWeekyear()));
+        logger.debug("星期{}", String.valueOf(now.getDayOfWeek()));
+        logger.debug("当月第{}天", String.valueOf(now.getDayOfMonth()));
+        logger.debug("当年第{}天", String.valueOf(now.getDayOfYear()));
+    }
+
+    /**
+     * 演示 DateTime 对象的格式化
+     */
+    @Test
+    public void testFormat() {
+        logger.debug("format date = {}", jodaUtils.getDateTime().toString("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    /**
+     * 演示 DateTime 对象的计算
+     */
+    @Test
+    public void testCalculate() {
+        // 计算2012年每个月的第一个周六的日期
+        jodaUtils = new JodaUtils(2012, 1, 1, 0, 0, 0);
+        for(int i = 1; i <= 12; i++){
+            // 获取每个月第一个星期的周六
+            DateTime dateTime = jodaUtils.setMonth(i).dayOfMonth().withMinimumValue().dayOfWeek().setCopy(6);
+            // 判断一个星期是否跨月
+            if(dateTime.getMonthOfYear() != i) {
+                // 如果跨月增加一个星期
+                dateTime = dateTime.plusWeeks(1);
+            }
+            logger.debug("date = {}", dateTime.toString("yyyy-MM-dd"));
+        }
+    }
+
+    /**
+     * DateTime 与 java.util.Date / java.util.Calendar
+     */
+    @Test
+    public void testConvert() {
+        DateTime dateTime = jodaUtils.getDateTime();
+        java.util.Date date = dateTime.toDate();
+        logger.debug("Date : {}", date.toString());
+        java.util.Calendar calendar = dateTime.toCalendar(Locale.CHINESE);
+        logger.debug("Calendar : {}", calendar.toString());
+    }
+}
