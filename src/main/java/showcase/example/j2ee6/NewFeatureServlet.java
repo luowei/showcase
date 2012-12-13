bda46ee1f601330d0a7ff6a2e3679af1c38bf228
zfc827 <zfc827@gmail.com>
2012-12-13 16:46:49 +0800

    同步git

---
+++ src/main/java/showcase/example/j2ee6/NewFeatureServlet.java
@@ -0,0 +1,29 @@
+package showcase.example.j2ee6;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author zfc827@gmail.com
+ */
+@WebServlet(name = "newServlet", urlPatterns = "/new", asyncSupported = true)
+public class NewFeatureServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        AsyncContext asyncContext = req.startAsync();
+        SlowResourceHandler slowResourceHandler = new SlowResourceHandler();
+        slowResourceHandler.setAsyncCtx(asyncContext);
+
+        asyncContext.start(slowResourceHandler);
+
+        resp.getWriter().println("servlet method executed...");
+        resp.getWriter().flush();
+
+    }
+}
---
+++ src/main/java/showcase/example/j2ee6/SlowResourceHandler.java
@@ -0,0 +1,43 @@
+package showcase.example.j2ee6;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author zfc827@gmail.com
+ */
+public class SlowResourceHandler implements Runnable {
+
+    private final Logger logger = LoggerFactory.getLogger(SlowResourceHandler.class);
+
+    private AsyncContext asyncCtx;
+
+    public void setAsyncCtx(AsyncContext asyncCtx) {
+        this.asyncCtx = asyncCtx;
+    }
+
+    @Override
+    public void run() {
+        logger.info("start asynchronous execute...");
+        PrintWriter writer = null;
+        try {
+            writer = asyncCtx.getResponse().getWriter();
+        } catch (IOException e) {
+        }
+        writer.println("start asynchronous execute...");
+        writer.flush();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+        }
+        writer.println("sleep 10 second continue execute...");
+        writer.flush();
+
+        asyncCtx.complete();
+    }
+}
